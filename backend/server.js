e26ebc92a8a8a0f7fcc857dab1f12ac99eff5f57
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-03-23 00:10:06 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+const app = express();
+const port = process.env.PORT || 3000 ;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express.js Server!</h1>');
+}); 
+
+app.listen(port,()=>{
+    console.log(`Server is running on the ${port}`)
+})
