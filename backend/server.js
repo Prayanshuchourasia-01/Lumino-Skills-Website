76535a5e26c1121630e9fae3c780779515da0e24
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-03-23 21:16:15 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
 
+
 const app = express();
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 4000 ;
+const mongoDbUrl = process.env.MONGO_DB_URL ; 
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 }); 
 
+
+
 app.listen(port,()=>{
     console.log(`Server is running on the ${port}`);
 })
+
+
+// mongoose.connect(mongoDbUrl)
