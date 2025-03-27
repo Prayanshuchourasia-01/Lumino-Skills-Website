0f4c0b7d3db9c9fb8e98d29601bd931edb62285c
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-03-27 09:05:55 +0530

    components folder created

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <>
+    <div className="container">
+        <img src="#" alt="Logo" className="logo" />
+        <div>
+            <a href="#" className="1element">Home</a>
+            <a href="#" className="2element">About</a>
+            <a href="#" className="3element">Contact</a>
+            <a href="#" className="4element">FillOut Form</a>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default navbar
