a541179eaaf4944ccbffad42acc90c1ec20bc6d8
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-04-05 17:14:00 +0530

    comments are added

--- backend/models/courseSchema.js
+++ backend/models/courseSchema.js
@@ -5,26 +5,32 @@ const courseSchema = mongoose.Schema(
     name:{
         type:'String',
         required:true
+        // name spelling limit can be 15 or 20 letter 
     },
     price:{
         type:'Number',
         required:true
+        // can be zero 
     },
     description:{
         type:'String',
         required:true
+        // limit upto 200 letters 
     },
     link:{
         type:'String',
         required:true
+        // url of the course 
     },
     tag:{
         type:'String',
         required:true
+        // tag are needed to filter out the courses to show the user
     },
     typeOfCourse:{
         type:'String',
         required:true
+        // in these tell which type of course it is , either free or paid || beginner or intermediate or expert.
     }
 },
 {
