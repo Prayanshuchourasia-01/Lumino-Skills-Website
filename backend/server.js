067074e9c2bbaeb034796e386b521fe2e77f4a1e
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-04-08 01:04:21 +0530

    Post Routes defined

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import Course from "./models/courseSchema.js";
 dotenv.config();
 
 
@@ -10,15 +10,32 @@ const port = process.env.PORT || 4000 ; /* Express Server Port Number  */
 // const mongoDbUrl =`mongodb+srv://${process.env.MONGO_DB_USERNAME2}:${process.env.MONGO_DB_PASSWORD2}@lumino-skills.z2jq2.mongodb.net/?retryWrites=true&w=majority&appName=Lumino-Skills`
 const DbUrl = process.env.MONGO_DB_URL; /* Mongo Db connection Url here */
 
+app.use(express.json());
+
+
 
 /* Routes defined here */
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 }); 
 
+app.post('/uploadCourse',async(req,res)=>{
+    try {
+        const course  = await Course.create(req.body)
+        res.status(200).json(course);
+
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 
 // console.log("Here is it ")
+
+
 
                                 /* Server and MongoDb Connection Establishment here */
 
