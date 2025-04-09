0605c24de57a5a010cad1c69c229d0924987b465
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-04-09 08:16:00 +0530

    updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {  fontFamily: {
+      'custom-font': ['Plus Jakarta Sans', 'sans-serif'], // Replace 'Roboto' with your font name
+    },},
   },
   plugins: [],
 }
