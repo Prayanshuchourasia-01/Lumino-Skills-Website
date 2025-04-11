e780b92d531e0248ff28029ff601536552554a2c
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-04-11 21:40:05 +0530

    Routing Is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,45 @@
-import Navbar from './components/Navbar';
 import './App.css';
-import CourseCard from './components/CourseCard';
-import Footer from './components/Footer';
-import FeatureSection from './components/FeatureSection';
-import ContactComponent from './components/ContactComponent';
-import PopularCourse from './components/PopularCourse';
-import HeroSection from './components/HeroSection';
 import Home from './Pages/Home';
+import CourseDetail from './components/CourseDetail';
+import CourseView from './components/CourseView';
+import FilterCourse from './components/FilterCourse';
+
+// const selectedCourse = {
+//   title: "Complete web development course",
+//   price: "3099",
+//   description:
+//     "Only web development course that you will need. Covers HTML, CSS, Tailwind, Node, React, MongoDB, Prisma, Deployment etc",
+//   CourseLink: "https://www.udemy.com/course/web-dev-master/",
+//   imageURL: "https://img-c.udemycdn.com/course/750x422/6035102_7d1a.jpg",
+//   tag: "Web Development",
+//   typeOfCourse: "PAID",
+//   CourseLevel: "Beginner",
+// };
+
+const selectedCourse = {
+  title: "Complete web development course",
+  price: "3099",
+  description: "Only web development course that you will need. Covers HTML, CSS, Tailwind, Node, React, MongoDB, Prisma, Deployment etc",
+  CourseLink: "https://www.udemy.com/course/web-dev-master/",
+  imageURL: "https://img-c.udemycdn.com/course/750x422/6035102_7d1a.jpg",
+  tag: "Web Development",
+  typeOfCourse: "PAID",
+  CourseLevel: "Beginner",
+  createdAt: "2025-04-07T17:02:06.634Z",
+  updatedAt: "2025-04-10T05:13:43.194Z"
+};
 
 function App() {
   return (
     <>
 
-<Home/>
+{/* <Home/> */}
 
+
+{/* <CourseDetail course={selectedCourse}/>
+<CourseView/>
+
+<FilterCourse/> */}
 
    
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Route, Routes} from 'react-router'
 import reportWebVitals from './reportWebVitals';
+import Home from './Pages/Home';
+import Free_Course_Page from './Pages/Free_Course_Page';
+import Paid_Course_Page from './Pages/Paid_Course_Page';
+import Categories_Page from './Pages/Categories_Page';
+import ContactPage from './Pages/ContactPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/freeCourse' element={<Free_Course_Page/>} />
+    <Route path='/paidCourse' element={<Paid_Course_Page/>} />
+    <Route path='/categories' element={<Categories_Page/>} />
+    <Route path='/contact' element={<ContactPage/>} />
+  </Routes>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
