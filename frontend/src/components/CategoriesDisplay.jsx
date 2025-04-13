003c8207c22add897aeb725340b3684f20dd23da
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-04-14 00:32:22 +0530

    categories display component create due to filter functionality is not completed & scrolltoTOp function added

---
+++ frontend/src/components/CategoriesDisplay.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Caegories_Display = () => {
+
+  return (
+    <div className=" bg-black text-white">
+ 
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+        <h2 className="text-4xl font-bold text-center mb-12 text-gray-400 hover:text-white">Explore Our Courses</h2>
+
+    
+        <div className=" gap-4 text-center text-lg font-medium text-gray-200 mb-12">
+          <div>
+            <h3 className="text-xl font-semibold mb-2">🧠 Skills</h3>
+            <p>Python, Web Development, C++, Java, C, Cloud Computing, Android Dev, Blockchain.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Caegories_Display;
