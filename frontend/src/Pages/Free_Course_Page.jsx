79ee1ee7f488516a777deaa55db0ec2dc1d0e835
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2025-04-13 20:14:54 +0530

    Free Course Page Completed

--- frontend/src/Pages/Free_Course_Page.jsx
+++ frontend/src/Pages/Free_Course_Page.jsx
@@ -3,10 +3,11 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import FilterCourse from '../components/FilterCourse';
 import axios from 'axios';
+import CourseCard from '../components/CourseCard';
 
 // const TypeOfCourseUser = 'FREE';
 const Free_Course_Page = () => {
-  const [Course,setCourses]=useState([]);
+  const [Courses,setCourses]=useState([]);
   useEffect(()=>{
       const getFreeCourse = async ()=> {
         try {
@@ -16,13 +17,24 @@ const Free_Course_Page = () => {
           console.log(error)
         }
       }
+      getFreeCourse()
   })
 
   return (
     <div>
       <Navbar/>
       <FilterCourse/>
-      
+
+<div className="h-fit justify-items-center  bg-slate-300 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+       
+        {Courses.map((course) => {
+        return  <CourseCard course = {course} />;
+          // console.log(course.CourseLevel);
+        })}
+
+        
+      </div>
+
       <Footer/>
     </div>
   )
